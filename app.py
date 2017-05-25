c2f6d1809af76798595efc248fbb37590c8c7b5f
Yingchun Zhang <200zyc@163.com>
2017-05-25 17:22:35 +0800

    111

--- app.py
+++ app.py
@@ -47,8 +47,7 @@ def makeWebhookResult(req):
     
         "basicCard": {
             "title": "Math & prime numbers",
-            "formattedText": "42 is an even composite number. It \n      is composed of three distinct prime numbers multiplied together. It \n      has a total of eight divisors. 42 is an abundant number, because the \n      sum of its proper divisors 54 is greater than itself. To count from \n      1 to 42 would take you about twenty-one…",
-            "image": {
+            "formattedText": "42 is an even composite number."
                 "url": "https://www.google.com/search?q=42",
                 "accessibilityText": "Image alternate text"
             },

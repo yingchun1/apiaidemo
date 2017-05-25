febcaaaee79d0aff2fcea05dd314606337b333c9
Yingchun Zhang <200zyc@163.com>
2017-05-25 17:10:44 +0800

    9

--- app.py
+++ app.py
@@ -44,7 +44,17 @@ def makeWebhookResult(req):
     return {
         "speech": speech,
         "displayText": speech,
-        #"data": {},
+        "data": {
+                            "basicCard": {
+                                "title": "Math & prime numbers",
+                                "formattedText": "42 is an even composite number. It \n      is composed of three distinct prime numbers multiplied together. It \n      has a total of eight divisors. 42 is an abundant number, because the \n      sum of its proper divisors 54 is greater than itself. To count from \n      1 to 42 would take you about twenty-one…",
+                                "image": {
+                                    "url": "https://www.google.com/search?q=42",
+                                    "accessibilityText": "Image alternate text"
+                                },
+                                "buttons": []
+                            }
+                        },
         # "contextOut": [],
         "source": "apiai-onlinestore-shipping"
     }
